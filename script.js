15f6ba96315e0c202ab08e12de0812847e41ff28
Kristen Ruprecht <ksruprec@gmail.com>
2022-03-10 23:20:14 -0500

    adding tasks and saving to local storage

--- script.js
+++ script.js
@@ -3,7 +3,12 @@
 //localStorage.setItem(key, value)
 //value = localStorage.getItem(key)
 
+//local storage variables
 const nameKey = "name"
+const todoKey = "todos"
+
+let todoList = []
+
 const monthsObj = {
     0: "Jan",
     1: "Feb",
@@ -18,6 +23,7 @@ const monthsObj = {
     10: "Nov",
     11: "Dec",
 }
+
 const currentDate = new Date()
 const month = currentDate.getMonth()
 const day = currentDate.getDate()
@@ -25,40 +31,88 @@ console.log(currentDate)
 console.log(month)
 console.log(day)
 
-
-
 window.onload = () => {
-    document.getElementById('date').innerHTML = `${monthsObj[month]} ${day}`
-    let greetingOutput = document.getElementById("output");
-    if (!greetingOutput)
-        return;
+    document.getElementById('date').innerHTML = `${monthsObj[month]} ${day}`;
 
-    //look for a name value in local storage
-    let name = localStorage.getItem(nameKey)
-    if (name !== null) {
-        //initialize greeting output if we already have a name value
-        greetingOutput.innerHTML = "Hello, " + name + "!"
-    } else {
-        greetingOutput.innerHTML = "Hello, friend!"
-    }
-
-    //takes two args: event it's listening for, and function that runs when event is triggered
-    greetingOutput.addEventListener("click", () => {
-        //clear greeting when we click on it
-        greetingOutput.innerHTML = ""
-    })
+    loadTodos()
 }
 
-const showGreeting = () => {
-    let nameInput = document.getElementById("input-name")
-    let greetingOutput = document.getElementById("output")
-    if (nameInput && greetingOutput) {
-        let name = nameInput.value
+//load list of todos in local storage
+const loadTodos = () => {
+    //get tasks from local storage
+    let todos = JSON.parse(localStorage.getItem(todoKey))
+    console.log(todos)
+    if (todos) {
+        console.log(todos)
+
+        todos.forEach(todo => {
+            let list = document.getElementById("todo-list")
+            let li = document.createElement('li');
+            li.innerHTML = '<li>' + todo + '</li>'
+            list.insertBefore(li, list.children[0]);
+        })
+    }
+    
+}
+
+//add new tasks to list
+const addTodos = () => {
+    let list = document.getElementById("todo-list")
+    let inputTodo = document.getElementById("input-todo").value;
+  
+    let li = document.createElement('li');
+    li.classList.add("tasks");
+    li.innerHTML = inputTodo;
+    document.getElementById("todo-list").appendChild(li);
+    console.log(inputTodo)
+
+    todoList.push(inputTodo)
+    localStorage.setItem(todoKey, JSON.stringify(todoList));
+
+    list.insertBefore(li, list.children[0]);
+    // clear input
+    task.value = "";
+    
+}
+
+const saveTodos = () => {
+    let tasks = document.getElementsByClassName("tasks")
+    todoList.push(tasks)
+    localStorage.setItem(todoKey, JSON.stringify(todoList))
+}
+
+// const showGreeting = () => {
+//     let nameInput = document.getElementById("input-name")
+//     let greetingOutput = document.getElementById("output")
+//     if (nameInput && greetingOutput) {
+//         let name = nameInput.value
         
-        //store name in local storage
-        localStorage.setItem(nameKey, name)
+//         //store name in local storage
+//         localStorage.setItem(nameKey, name)
 
-        //greet the user
-        greetingOutput.innerHTML = "Hello " + nameInput.value + "!"
-    }
-}
+//         //greet the user
+//         greetingOutput.innerHTML = "Hello " + nameInput.value + "!"
+//     }
+// }
+
+// window.onload = () => {
+
+//     let greetingOutput = document.getElementById("output");
+//     if (!greetingOutput)
+//         return;
+
+//     //look for a name value in local storage
+//     let name = localStorage.getItem(nameKey)
+//     if (name !== null) {
+//         //initialize greeting output if we already have a name value
+//         greetingOutput.innerHTML = "Hello, " + name + "!"
+//     } else {
+//         greetingOutput.innerHTML = "Hello, friend!"
+//     }
+
+//     //takes two args: event it's listening for, and function that runs when event is triggered
+//     greetingOutput.addEventListener("click", () => {
+//         //clear greeting when we click on it
+//         greetingOutput.innerHTML = ""
+//     })
+// }
